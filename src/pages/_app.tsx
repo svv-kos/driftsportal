e5577b59c9d72b5365b12f7029959c8ce8bd4376
Martin Solheim <martin.solheim@me.com>
2021-10-27 16:39:18 +0200

    Legger til cookie-banner
    Co-authored-by: Nina Kjekstad <ninakjeks@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,39 @@
 import type { AppProps } from "next/app";
 import { hotjar } from "react-hotjar";
 import "./styles.css";
-import { useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import CookieBanner from "react-cookie-banner";
+
+interface CookieWrapperProps {
+  setBrukHotjar: (brukHotjar: boolean) => void;
+  children: ReactNode;
+}
+
+const CookieWrapper = ({ setBrukHotjar, children }: CookieWrapperProps) => {
+  return (
+    <>
+      <CookieBanner
+        message="Denne siden bruker cookies for å gi en bedre brukeropplevelse, hvis du fortsetter å bruke siden aksepterer du cookies"
+        onAccept={() => {
+          setBrukHotjar(true);
+        }}
+        cookie="user-has-accepted-cookies"
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [brukHotjar, setBrukHotjar] = useState(false);
   useEffect(() => {
-    hotjar.initialize(2671965, 6);
+    brukHotjar ? hotjar.initialize(2671965, 6) : null;
   });
-  return <Component {...pageProps} />;
+  return (
+    <CookieWrapper setBrukHotjar={setBrukHotjar}>
+      <Component {...pageProps} />
+    </CookieWrapper>
+  );
 }
+
 export default MyApp;
