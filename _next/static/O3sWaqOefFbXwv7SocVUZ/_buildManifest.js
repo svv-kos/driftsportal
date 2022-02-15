fb7c815aae0e51013b24739d4b53ceae3a3f70ba
ninakjek <ninakjek@users.noreply.github.com>
2022-02-15 11:47:02 +0000

    Deploying to gh-pages from @ svv-kos/driftsportal@e0cb2e5a94d724580308a0f2b233c9ec7d01e88f 🚀

---
+++ _next/static/O3sWaqOefFbXwv7SocVUZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8a9299325e84e781223d.css","static/chunks/pages/index-a72812ab485096725817.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
